2609c3a587b74828d8b7a9f7cc0ee6a497ad62bd
GwangJin Byeon <qusrhkdwls45@gmail.com>
2023-10-28 19:57:50 +0900

    간단한 투두리스트 만들기 1
    투두 데이터를 관리할 수 있도록 만들어 봤음.

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,7 +1,32 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDo("");
+    setToDos((currentArray) => {
+      return [toDo, ...currentArray];
+    });
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type={"text"}
+          placeholder="Write some"
+        ></input>
+        <button>Add to do</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
