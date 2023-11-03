4d52ac557cc1e83a4867f2edccab39158f0c4cf0
GwangJin Byeon <qusrhkdwls45@gmail.com>
2023-11-03 16:59:46 +0900

    movie 컴포넌트화 시키기 까지 완료

--- movie_app/src/routes/Detail.js
+++ movie_app/src/routes/Detail.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [movie, setMovie] = useState({});
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+
+    // setMovie((prev) => ({ ...prev, ...json.data.movie }));
+    setMovie(json.data.movie);
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <h1>
+      {loading ? (
+        <div>
+          <img src={movie.url}></img>
+        </div>
+      ) : (
+        "Loading..."
+      )}
+    </h1>
+  );
 }
 
 export default Detail;
