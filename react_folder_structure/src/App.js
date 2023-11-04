9ac148f59d1c4422c3645e3f63c1992dc336ea18
GwangJin Byeon <qusrhkdwls45@gmail.com>
2023-11-04 17:35:45 +0900

    하나의 파일안에 여러개의 컴포넌트 만들기
    - 처음엔, 혹은 간단한 컴포넌트는 이렇게 할수도 있다.
    - 하지만 혼자 개발하는 것도 아니고 이렇게 했다가 무슨 일이 생길 지 모르는 법이다.
    - 다음으로는 파일을 여러개의 파일로 분리 해볼 것이다.

--- react_folder_structure/src/App.js
+++ react_folder_structure/src/App.js
@@ -1,7 +1,32 @@
-function App() {
-  const title = 'React';
+const list = [
+  {
+    id: 'a',
+    firstname: 'Robin',
+    lastname: 'Wieruch',
+    year: 1988,
+  },
+  {
+    id: 'b',
+    firstname: 'Dave',
+    lastname: 'Davidds',
+    year: 1990,
+  },
+];
+function ListItem({listItem}){
+  return <li>{listItem.firstname}</li>
+};
 
-  return (<div><h1>Hello {title}</h1></div>);
+function List({list}){
+  return <ul>
+    {list.map((item) => <ListItem key={item.id} listItem={item}></ListItem>)}
+  </ul>
+};
+
+function App() {
+
+  return <div>
+    <List list={list}></List>
+  </div>;
 }
 
 export default App;
